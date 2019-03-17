431f69a442efa5ed5d52313bca76071fb4daac5a
Kevin <kfeddema25@gmail.com>
2019-03-16 23:26:43 -0700

    Finished getting all valid moves || Cinstructed array list used for state children

--- src/main/java/cosc322/Utility.java
+++ src/main/java/cosc322/Utility.java
@@ -22,4 +22,16 @@ public class Utility {
     public void print(int i){
         System.out.println(i);
     }
+    
+    public void print(boolean b){
+        System.out.println(b);
+    }
+    
+    public int[][] copyBoard(int [][] b){
+        int[][] copy = new int[11][11];
+        for(int i = 0; i < 11; i++){
+            copy[i] = b[i].clone();
+        }
+        return copy;
+    }
 }
